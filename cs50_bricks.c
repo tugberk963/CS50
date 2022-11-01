f16d470b00b46a1df092e88ebff0379589ceded6
tugberk963 <102547030+tugberk963@users.noreply.github.com>
2022-11-01 22:27:22 +0300

    CS50x2022 Lecture 1 - C - Exercise 1 Solution.

---
+++ cs50_bricks.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height; // Define Height
+
+    do
+    {
+        height = get_int("height: "); // Ask User
+    }
+    while(height < 1); // While Height Less Than 1 --> Ask Again
+
+    for (int rows = 0; rows < height; rows++)   // Creating rows till reaching desired height
+    {
+        for (int blanks = height - rows; blanks >= 0; blanks --) // Usign blanks for left alignin blocks.
+        {
+        printf(" ");
+        }
+        for(int bricks = 0; bricks <= rows; bricks++) // Filling rows with bricks.
+        {
+            printf("#");
+        }
+        printf("\n"); // Step on new row.
+    }
+}
